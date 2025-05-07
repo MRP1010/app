f0a4f3040c0914166fd89b62eecf461f7f5fbcb6
MRP1010 <mreynosos1@gmail.com>
2025-05-07 11:20:44 -0600

    Tercer commit añadiendo funciones

--- app.py
+++ app.py
@@ -4,20 +4,44 @@ import streamlit as st
 
 st.header('Proyecto 7 Aplicación Web')
 
-data_cars = pd.read_csv('vehicles_us.csv')               
+data_cars = pd.read_csv('vehicles_us.csv')   # leer los datos           
 
-crea_hist = st.button('Generar Histograma') 
+crea_hist = st.button('Generar Histograma')  # crear un botón
 
-if crea_hist:
-   
-   fig = px.histogram(data_cars, x="odometer")
+if crea_hist: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    # crear un histograma
+    fig = px.histogram(data_cars, x="odometer")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
 
-   st.plotly_chart(fig, use_container_width=True)
+crea_dist = st.button('Generar gráfico de dispersión')  # crea el segundo  botón
 
-crea_dist = st.button('Genera gráfico de dispersión')  
-
-if crea_dist:  
-
+if crea_dist:# al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Gráfico de dispersión')
+    # crear la distribución
     fig = px.scatter(data_cars, x="odometer", y="price")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
 
+crea_hist = st.checkbox('Seleccione para generar el histograma')  # crear un botón
+
+if crea_hist: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    # crear un histograma
+    fig = px.histogram(data_cars, x="odometer")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+crea_dist = st.checkbox('Seleccione para construir gráfico de dispersión')  # crea el segundo  botón
+
+if crea_dist:# al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Gráfico de dispersión')
+    # crear la distribución
+    fig = px.scatter(data_cars, x="odometer", y="price")
+    # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
